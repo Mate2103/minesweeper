516a77363b46d1fc929cc1b83d972f62c5b2ccb6
Szabó Máté <nohab2005@gmail.com>
2022-10-24 16:46:35 +0200

    bombs ready, numbers in progress

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const newgame = document.querySelector('.newgame');
 const difficulty = document.querySelector('#difficulty');
 const map_container = document.querySelector('.map-container');
 let bombs = [];
-let tiles = [,];
+let tiles = [];
 let random = 0;
 let random2 = 0;
 
 function generateMap(e) {
     bombs = [];
-    tiles = [,];
+    tiles = [];
     while (map_container.firstChild) {
         map_container.removeChild(map_container.lastChild);
     }
@@ -19,33 +19,47 @@ function generateMap(e) {
         t = [random, random2]
         bombs.push(t);
     }
-    console.log(bombs);
+    let k = 0;
     for (let i = 0; i < 9; i++) {
+        tiles[i] = [];
         for (let j = 0; j < 9; j++) {
             const element = document.createElement('div');
             map_container.appendChild(element);
             element.classList.add('map-element');
 
-            let k = bombs.some(function (item) { return item[0] == i })
-            let m = bombs.some(function (item) { return item[1] == j })
-            if (k && m) {
-                element.classList.add('bomb');
-                element.innerHTML = "&#128163;";
-            }
+            bombs.forEach(bomb => {
+                if (bomb[0] == i && bomb[1] == j) {
+                    element.classList.add('bomb');
+                    element.innerHTML = "&#128163;";
+                }
+            });
             element.addEventListener('click', tileClick);
-            tiles[i, j] = element;
+            tiles[i][j] = element;
         }
     }
     console.log(tiles);
-    for (let i = 0; i < 9 * 9; i++) {
-        if (i + 9 > 80) {
-            if (i - 1 <= 80 - 9) {
-                for (let j = 0; j < 3; j++) {
+    //kill me
+    let bombcount = 0;
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            bombcount = 0;
+            if (i - 1 < 0) continue;
+            if (j - 1 < 0) continue;
+            if (i + 1 > 8) continue;
+            if (j + 1 > 8) continue;
+            if (!(i - 1 < 0 && j - 1 < 0)) if (tiles[i - 1][j - 1].classList.contains('bomb')) bombcount++;
+            if (!(j + 1 > 8)) if (tiles[i][j + 1].classList.contains('bomb')) bombcount++;
+            if (!(i + 1 > 8 && j + 1 > 8)) if (tiles[i + 1][j + 1].classList.contains('bomb')) bombcount++;
+            if (!(i - 1<0 && j + 1 > 8)) if (tiles[i - 1][j + 1].classList.contains('bomb')) bombcount++;
+            if (!(i + 1 > 8 && j-1<0)) if (tiles[i + 1][j - 1].classList.contains('bomb')) bombcount++;
 
-                }
-            }
+           if (!(i+1)) if (tiles[i + 1][j].classList.contains('bomb')) bombcount++;
+            if (tiles[i - 1][j].classList.contains('bomb')) bombcount++;
+
+            if (bombcount > 0 && !(tiles[i][j].classList.contains('bomb'))) tiles[i][j].innerHTML = bombcount;
         }
     }
+
 }
 
 function tileClick(e) { e.target.classList.add('taken'); }
@@ -55,5 +69,4 @@ function main() {
 }
 
 newgame.addEventListener('click', generateMap);
-console.log(tiles);
 main();
