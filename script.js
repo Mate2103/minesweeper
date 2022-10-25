3e17c9f4519c9cbb56a4c186d18fb2dbcea53bae
Szabó Máté <nohab2005@gmail.com>
2022-10-25 13:26:47 +0200

    game almost done

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const newgame = document.querySelector('.newgame');
 const difficulty = document.querySelector('#difficulty');
 const map_container = document.querySelector('.map-container');
 let bombs = [];
+let bombcountgenerate = 0;
 let tiles = [];
 let random = 0;
 let random2 = 0;
@@ -16,20 +17,33 @@ let colors = {
     7: 'black',
     8: 'gray'
 };
+let takenTiles = 0;
+
+//bomba: easy: 1:7 medium: 1:6 hard: 1:5
 
 function generateMap(e) {
+    onSelectChange();
+    takenTiles = 0;
     died = false;
     bombs = [];
     tiles = [];
     while (map_container.firstChild) {
         map_container.removeChild(map_container.lastChild);
     }
-    let t = [random, random2];
-    for (let i = 0; i < 10; i++) {
-        random = Math.floor(8 * Math.random());
-        random2 = Math.floor(8 * Math.random());
-        t = [random, random2]
-        bombs.push(t);
+    let coordinates = [];
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            coordinates.push([i, j]);
+        }
+    }
+    for (let i = coordinates.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = coordinates[i];
+        coordinates[i] = coordinates[j];
+        coordinates[j] = temp;
+    }
+    for (let i = 0; i < bombcountgenerate; i++) {
+        bombs.push(coordinates[i]);
     }
     let k = 0;
     for (let i = 0; i < 9; i++) {
@@ -90,23 +104,44 @@ function tileClick(e) {
     if (e.target.innerHTML.length == 0) {
         let i = tiles.findIndex(arr => arr.includes(e.target));
         let j = tiles[i].indexOf(e.target);
-        revealEmpty(i, j);
+        revealTile(i, j);
     }
 }
 
-function revealEmpty(i, j) {
+function revealTile(i, j) {
+    if (tiles[i][j].classList.contains('emptytaken')) return;
     tiles[i][j].classList.add('taken');
+    if (tiles[i][j].classList.length > 2) return;
+    tiles[i][j].classList.add('emptytaken');
     //ide is jöhetne valami jobb, de ez van
-    if (!(i - 1 < 0) && !(j - 1 < 0)) if (tiles[i - 1][j - 1].innerHTML == "") revealEmpty(i-1, j-1);
-    if (!(i - 1 < 0) && !(j + 1 > 8)) if (tiles[i - 1][j + 1].innerHTML=="") revealEmpty(i-1, j+1);
-    if (!(i + 1 > 8) && !(j - 1 < 0)) if (tiles[i + 1][j - 1].innerHTML=="") revealEmpty(i+1, j-1);
-    if (!(i + 1 > 8) && !(j + 1 > 8)) if (tiles[i + 1][j + 1].innerHTML == "") revealEmpty(i+1, j+1);
+    if (!(i - 1 < 0) && !(j - 1 < 0)) revealTile(i - 1, j - 1);
+    if (!(i - 1 < 0) && !(j + 1 > 8)) revealTile(i - 1, j + 1);
+    if (!(i + 1 > 8) && !(j - 1 < 0)) revealTile(i + 1, j - 1);
+    if (!(i + 1 > 8) && !(j + 1 > 8)) revealTile(i + 1, j + 1);
 
-    if (!(j + 1 > 8)) if (tiles[i][j + 1].innerHTML=="") revealEmpty(i, j+1);
-    if (!(j - 1 < 0)) if (tiles[i][j - 1].innerHTML=="") revealEmpty(i, j-1);
-    if (!(i + 1 > 8)) if (tiles[i + 1][j].innerHTML=="") revealEmpty(i+1, j);
-    if (!(i - 1 < 0)) if (tiles[i - 1][j].innerHTML =="") revealEmpty(i-1, j);
+    if (!(j + 1 > 8)) revealTile(i, j + 1);
+    if (!(j - 1 < 0)) revealTile(i, j - 1);
+    if (!(i + 1 > 8)) revealTile(i + 1, j);
+    if (!(i - 1 < 0)) revealTile(i - 1, j);
 }
 
+function onSelectChange() {
+    switch (difficulty.value) {
+        case 'easy':
+            bombcountgenerate = 10;
+            break;
+        case 'medium':
+            bombcountgenerate = 12;
+            break;
+        case 'hard': 
+            bombcountgenerate = 14;
+            break;
+    }
+}
+
+function addFlag(e) {
+ alert('fasz');
+}
 newgame.addEventListener('click', generateMap);
+difficulty.addEventListener('change', generateMap);
 generateMap();
