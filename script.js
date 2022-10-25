f35eaf35fb356a2610c199be45a408972e70c026
Máté <nohab2005@gmail.com>
2022-10-25 21:33:19 +0200

    Done expect features

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const newgame = document.querySelector('.newgame');
 const difficulty = document.querySelector('#difficulty');
 const map_container = document.querySelector('.map-container');
+const target = document.querySelector('.picktile');
+const flag = document.querySelector('.flag');
+const flagsremained = document.querySelector('.flags-remained');
 let bombs = [];
 let bombcountgenerate = 0;
 let tiles = [];
 let random = 0;
 let random2 = 0;
 let died = false;
+let flagorpick = false; //false - pick, true - flag
 let colors = {
     1: 'blue',
     2: 'green',
@@ -18,6 +22,7 @@ let colors = {
     8: 'gray'
 };
 let takenTiles = 0;
+let numberofflags = 0;
 
 //bomba: easy: 1:7 medium: 1:6 hard: 1:5
 
@@ -45,6 +50,8 @@ function generateMap(e) {
     for (let i = 0; i < bombcountgenerate; i++) {
         bombs.push(coordinates[i]);
     }
+    numberofflags = bombcountgenerate
+    flagsremained.innerHTML = numberofflags;
     let k = 0;
     for (let i = 0; i < 9; i++) {
         tiles[i] = [];
@@ -56,7 +63,7 @@ function generateMap(e) {
             bombs.forEach(bomb => {
                 if (bomb[0] == i && bomb[1] == j) {
                     element.classList.add('bomb');
-                    element.innerHTML = "&#128163;";
+                    element.innerHTML = '<img src="./imgs/mine.png" alt="mine" class="bomb-image">';
                 }
             });
             element.addEventListener('click', tileClick);
@@ -91,29 +98,46 @@ function generateMap(e) {
 function tileClick(e) {
     if (died) return;
     if (e.target.classList.contains('taken')) return;
-    e.target.classList.add('taken');
-    if (e.target.classList.contains('bomb')) {
+    if (flagorpick) {
+        if (numberofflags > 0 && !e.target.classList.contains('flagged')) {
+            e.target.classList.add('flagged');
+            numberofflags--;
+            flagsremained.innerHTML = numberofflags;
+            return;
+        }
+        if (e.target.classList.contains('flagged')) {
+            e.target.classList.remove('flagged');
+            numberofflags++;
+            flagsremained.innerHTML = numberofflags;
+        }
+    } else {
+        if ( e.target.classList.contains('flagged')) return;
         e.target.classList.add('taken');
-        died = true;
-        for (let i = 0; i < 9; i++) {
-            for (let j = 0; j < 9; j++) {
-                if (tiles[i][j].classList.contains('bomb')) tiles[i][j].classList.add('taken');
+        checkForWin();
+        if (e.target.classList.contains('bomb')) {
+            e.target.classList.add('taken');
+            died = true;
+            for (let i = 0; i < 9; i++) {
+                for (let j = 0; j < 9; j++) {
+                    if (tiles[i][j].classList.contains('bomb')) tiles[i][j].classList.add('taken');
+                }
             }
         }
-    }
-    if (e.target.innerHTML.length == 0) {
-        let i = tiles.findIndex(arr => arr.includes(e.target));
-        let j = tiles[i].indexOf(e.target);
-        revealTile(i, j);
+        if (e.target.innerHTML.length == 0) {
+            let i = tiles.findIndex(arr => arr.includes(e.target));
+            let j = tiles[i].indexOf(e.target);
+            revealTile(i, j);
+        }
     }
 }
 
 function revealTile(i, j) {
-    if (tiles[i][j].classList.contains('emptytaken')) return;
+    if (tiles[i][j].classList.contains('emptytaken') 
+        || tiles[i][j].classList.contains('flagged')) return;
     tiles[i][j].classList.add('taken');
     if (tiles[i][j].classList.length > 2) return;
     tiles[i][j].classList.add('emptytaken');
-    //ide is jöhetne valami jobb, de ez van
+    //ide is jöhetne valami jobb, de ez van egyelőre
     if (!(i - 1 < 0) && !(j - 1 < 0)) revealTile(i - 1, j - 1);
     if (!(i - 1 < 0) && !(j + 1 > 8)) revealTile(i - 1, j + 1);
     if (!(i + 1 > 8) && !(j - 1 < 0)) revealTile(i + 1, j - 1);
@@ -133,15 +157,45 @@ function onSelectChange() {
         case 'medium':
             bombcountgenerate = 12;
             break;
-        case 'hard': 
+        case 'hard':
             bombcountgenerate = 14;
             break;
     }
 }
-
-function addFlag(e) {
- alert('fasz');
+function flagSelected(e) {
+    if (!flagorpick) {
+        flagorpick = true;
+        flag.classList.toggle('selected-button');
+        target.classList.toggle('selected-button');
+    }
 }
+function targetSelected(e) {
+    if (flagorpick) {
+        flagorpick = false;
+        flag.classList.toggle('selected-button');
+        target.classList.toggle('selected-button');
+    }
+}
+function checkForWin() {
+    let takens = 0;
+    for (let i = 0; i < tiles.length; i++) {
+        for (let j = 0; j < tiles.length; j++) {
+            if (tiles[i][j].classList.contains('taken')) takens++;
+        }
+    }
+    if (9*9-bombcountgenerate == takens) Win();
+    console.log(takens);
+}
+function Win() {
+    let win = document.createElement('p');
+    win.innerHTML = "You won!";
+    win.classList.add('win');
+    document.body.appendChild(win);
+}
+
 newgame.addEventListener('click', generateMap);
 difficulty.addEventListener('change', generateMap);
+flag.addEventListener('click', flagSelected);
+target.addEventListener('click', targetSelected);
+
 generateMap();
